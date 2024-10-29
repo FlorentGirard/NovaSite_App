9387d84d3a0698c7976750f981ef1ce2e17ad6ae
Florent.G <mrgirardflorent@gmail.com>
2024-10-29 21:13:36 +0100

    chore(config):add talwind and configure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    css: ['~/assets/css/talwind.css', '~/assets/css/custom-styles-talwind.css'],
+    modules: ['@nuxtjs/tailwindcss'],
 })
